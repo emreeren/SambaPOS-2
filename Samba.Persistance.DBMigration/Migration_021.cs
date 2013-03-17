4a86cba36edae69604a0c8a538a8ce19154a2881
Emre Eren <emreeren@gmail.com>
2013-03-17 19:37:34 +0200

    TimeCardAction field removed from migration class

--- Samba.Persistance.DBMigration/Migration_021.cs
+++ Samba.Persistance.DBMigration/Migration_021.cs
@@ -25,35 +25,6 @@ namespace Samba.Persistance.DBMigration
 		    Create.Column("ContactPhone").OnTable("Users").AsString(128).Nullable();
 		    Create.Column("EmergencyPhone").OnTable("Users").AsString(128).Nullable();
 		    Create.Column("DateOfBirth").OnTable("Users").AsString(128).Nullable();
-		    Create.Column("TimeCardAction").OnTable("Users").AsInt32().WithDefaultValue(0);
-            
-
-        //         public string ContactPhone { get; set; }
-        //public string EmergencyPhone { get; set; }
-        //public string DateOfBirth { get; set; }
-        //public int TimeCardAction { get; set; } 
-
-//            var script = @"
-//SET ANSI_NULLS ON
-//GO
-//
-//SET QUOTED_IDENTIFIER ON
-//GO
-//
-//CREATE TABLE [dbo].[TimeCardEntries](
-//	[Id] [int] IDENTITY(1,1) NOT NULL,
-//	[User_Id] [int] NOT NULL,
-//	[Action] [int] NOT NULL,
-//	[DateTime] [datetime] NOT NULL,
-//	[Name] [nvarchar](50) NULL,
-// CONSTRAINT [PK_TimeCardEntries] PRIMARY KEY CLUSTERED 
-//(
-//	[Id] ASC
-//)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-//) ON [PRIMARY]
-//
-//GO";
-//            Execute.EmbeddedScript(script);
 		}
 
 		public override void Down()
